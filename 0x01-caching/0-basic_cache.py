38595013c00d7fb4b238a05c9e9da865367d0f43
El-sharqawy <elsharqawy2@gmail.com>
2024-08-22 00:30:08 +0300

    Basic Cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""basic cache class"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """basic cache class"""
+
+    def __init__(self):
+        """class constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """adds item to the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """return the value in self.cache_data, otherwise None"""
+        return self.cache_data.get(key, None)
